286827e28e414e392ec13666678e6da19be006a1
rainbowdwarf <glubokov3099@gmail.com>
2023-03-31 00:11:52 +0700

    add new urls

--- blog/news/urls.py
+++ blog/news/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views
 
 from .views import *
-
+from .user_views import *
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+
+    path('registration/', SignUpView.as_view(), name='registration'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
