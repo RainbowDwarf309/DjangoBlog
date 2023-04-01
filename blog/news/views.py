8f00ad37dcebc0dafcf51146d134bb41e6ee0e64
rainbowdwarf <glubokov3099@gmail.com>
2023-04-01 12:20:13 +0700

    add CreatePost,SinglePost,PostsByCategory, PostsByTag

--- blog/news/views.py
+++ blog/news/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from django.views.generic import CreateView, ListView, DetailView
+from django.contrib.auth.decorators import login_required
+from .models import Post, Category, Tag
+from .forms import CreatePostForm
+from django.utils.decorators import method_decorator
+from django.urls import reverse, reverse_lazy
 from django.db.models import F
 
 
@@ -14,3 +18,67 @@ class Home(ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Django Blog'
         return context
+
+
+class SinglePost(DetailView):
+    model = Post
+    template_name = 'news/single_post.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
+        return context
+
+
+class PostsByCategory(ListView):
+    template_name = 'news/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
+        return context
+
+
+class PostsByTag(ListView):
+    template_name = 'news/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Tag.objects.get(slug=self.kwargs['slug'])
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class CreatePost(CreateView):
+    form_class = CreatePostForm
+    template_name = 'news/create_post.html'
+    context_object_name = 'news'
+
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Django Blog'
+        context['form'] = self.form_class(initial={'author': self.request.user})
+        return context
+
+
+
+
+
+
