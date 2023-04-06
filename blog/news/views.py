210161f7c56aaaac8744c7fa4e40038be98a87ab
rainbowdwarf <glubokov3099@gmail.com>
2023-04-06 23:00:18 +0700

    optimized queries

--- blog/news/views.py
+++ blog/news/views.py
@@ -16,6 +16,9 @@ class Home(ListView):
     context_object_name = 'posts'
     paginate_by = 4
 
+    def get_queryset(self):
+        return Post.objects.filter(is_published=True).select_related('category').prefetch_related('tags')
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Django Blog'
