18a0fc9115339513e035b5633c514bd0779013be
rainbowdwarf <glubokov3099@gmail.com>
2023-04-20 17:58:06 +0700

    add url for new api

--- blog/news/api/urls.py
+++ blog/news/api/urls.py
@@ -5,8 +5,9 @@ from . import api_views
 from news.models import Post, FavoritePost
 
 urlpatterns = [
+    path('code_id/', api_views.get_post_view_api, name='ajax_post'),
     path('favorite_post/', api_views.get_favorites_view_api,
          kwargs={'model': FavoritePost, 'object_model': Post}, name='ajax_favorite_post'),
-    path('post_like_dislike/', api_views.get_like_or_dislike_view_api, name='ajax_post_like_dislike'),
+    path('post_like_dislike/', api_views.get_post_like_or_dislike_view_api, name='ajax_post_like_dislike'),
     path('comment_like_dislike/', api_views.get_comment_like_or_dislike_view_api, name='ajax_comment_like_dislike'),
 ]
