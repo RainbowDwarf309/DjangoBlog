78d81b273626f1f9c13bad9ba25fd3c68dafc335
rainbowdwarf <glubokov3099@gmail.com>
2023-04-20 17:56:53 +0700

    add get_post_view_api for updating post views and add track_action to existing APIs

--- blog/news/api/api_views.py
+++ blog/news/api/api_views.py
@@ -1,12 +1,65 @@
-from news.models import Post, Comment
+import logging
+from news.models import ActionTrack, Post, Comment
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 
-from django.http import JsonResponse
+from django.http import HttpRequest, JsonResponse
+
+from news.api.api_functions import (
+    check_object_id,
+    get_client_ip,
+    check_post_status,
+    check_comment_status,
+    post_already_viewed_today,
+    post_viewed,
+    post_change_rating,
+    track_action,
+)
+
+logger = logging.getLogger('process')
+
+
+def get_post_view_api(request: HttpRequest) -> JsonResponse:
+    """Change the number of views or rating of the post"""
+    post_id = request.GET.get('code_id', None)
+    action = request.GET.get('action', None)
+    try:
+        post_id = check_object_id(post_id)
+        post = Post.objects.get(pk=post_id)
+        user_changed_utility = ActionTrack.objects.filter(details=f'ip={get_client_ip(request)}, '
+                                                                  f'code_id={post_id}, u')
+        if post.status != Post.AttrStatus.APPROVED:
+            return check_post_status(request, post)
+        user_post_already_viewed_today = post_already_viewed_today(request, post_id)
+    except (ValueError, TypeError, AssertionError):
+        response = JsonResponse({'error': 'code_id must be integer or string of integer, and more or equal 0.'})
+        response.status_code = 400
+        logger.info(f'Block get_post_view_api. Return 400={response}')
+        return response
+    except ObjectDoesNotExist:
+        response = JsonResponse({'error': 'Object with code_id=' + str(post_id) + " does not exist."})
+        response.status_code = 400
+        logger.info(f'Block get_post_view_api. Return 400={response}, from ip={get_client_ip(request)}')
+        return response
+    except Exception as e:
+        response = JsonResponse({'error': 'Unexpected error'})
+        response.status_code = 400
+        logger.warning(f'Block get_post_view_api. Return 400={response}, from ip={get_client_ip(request)}, '
+                       f'Exc:{e}')
+        return response
+    else:
+        if action == 'viewed' and not user_changed_utility:
+            post_viewed(request, post, post_id, action, user_post_already_viewed_today)
+        else:
+            return post_change_rating(request, post, post_id, action, user_changed_utility)
+        response = JsonResponse({'rate': post.rating, 'views': post.views, 'state': 'viewed'})
+        logger.debug(f'Block get_post_view_api. Return response{response}, from ip={get_client_ip(request)}')
+        response.status_code = 200
+        return response
 
 
 @login_required
-def get_favorites_view_api(request, **kwargs):
+def get_favorites_view_api(request, **kwargs) -> JsonResponse:
     slug = request.GET.get('slug', "")
     try:
         model = kwargs['model']
@@ -37,21 +90,44 @@ def get_favorites_view_api(request, **kwargs):
 
 
 @login_required
-def get_like_or_dislike_view_api(request):
+def get_post_like_or_dislike_view_api(request: HttpRequest) -> JsonResponse:
     slug = request.GET.get('slug', "")
     action = request.GET.get('action', "")
     try:
         obj = Post.objects.get(slug=slug)
-        if action == 'like':
+        user_changed_utility = ActionTrack.objects.filter(details=f'ip={get_client_ip(request)}, '
+                                                                  f'code_id={obj.pk}, u')
+        if obj.status != Post.AttrStatus.APPROVED:
+            return check_post_status(request, obj)
+        if action == 'like' and not user_changed_utility:
             obj.set_like_or_dislike(is_like=True)
             obj.save()
-            response = JsonResponse({'status': 'Successful', 'state': 'like', 'rate': obj.rating})
+            track_action(request=request, action=ActionTrack.AttrAction.POST_RATING_PLUS,
+                         page=ActionTrack.AttrPage.API_LIKE_DISLIKE,
+                         details=f'ip={get_client_ip(request)}, code_id={obj.pk}, u')
+            response = JsonResponse({'status': 'Successful', 'state': 'like', 'rate': obj.rating, 'views': obj.views})
             response.status_code = 200
             return response
-        elif action == 'dislike':
+        elif action == 'dislike' and not user_changed_utility:
             obj.set_like_or_dislike(is_dislike=True)
             obj.save()
-            response = JsonResponse({'status': 'Successful', 'state': 'dislike', 'rate': obj.rating})
+            track_action(request=request, action=ActionTrack.AttrAction.POST_RATING_MINUS,
+                         page=ActionTrack.AttrPage.API_LIKE_DISLIKE,
+                         details=f'ip={get_client_ip(request)}, code_id={obj.pk}, u')
+            response = JsonResponse(
+                {'status': 'Successful', 'state': 'dislike', 'rate': obj.rating, 'views': obj.views}
+            )
+            response.status_code = 200
+            return response
+        elif action and user_changed_utility:
+            track_action(request=request, action_reason=ActionTrack.AttrReason.POST_RATING_ALREADY_CHANGED,
+                         page=ActionTrack.AttrPage.API_LIKE_DISLIKE,
+                         action=ActionTrack.AttrAction.POST_RATING_MINUS if action == 'dislike' else
+                         ActionTrack.AttrAction.POST_RATING_PLUS)
+            logger.debug(f'Block get_post_like_or_dislike_view_api Post rating change unsuccessfully '
+                         f'user_changed_utility, post id={obj.pk}, from ip={get_client_ip(request)}')
+            response = JsonResponse({'rate': obj.rating, 'views': obj.views,
+                                     'error': 'You already set like or dislike.'})
             response.status_code = 200
             return response
     except KeyError:
@@ -65,27 +141,46 @@ def get_like_or_dislike_view_api(request):
         response = JsonResponse({'error': 'Unexpected error'})
         response.status_code = 400
     else:
-        response = JsonResponse({'status': 'Successful', 'state': 'You already set like or dislike'})
-        response.status_code = 200
-        return response
+        response = JsonResponse({'error': 'Unexpected action.'})
+        response.status_code = 400
 
 
 @login_required
-def get_comment_like_or_dislike_view_api(request):
+def get_comment_like_or_dislike_view_api(request: HttpRequest) -> JsonResponse:
     code_id = request.GET.get('code_id', "")
     action = request.GET.get('action', "")
     try:
         obj = Comment.objects.get(pk=code_id)
-        if action == 'like':
+        user_changed_utility = ActionTrack.objects.filter(details=f'ip={get_client_ip(request)}, '
+                                                                  f'code_id={obj.pk}, u')
+        if obj.status != Comment.AttrStatus.VISIBLE:
+            return check_comment_status(request, obj)
+        if action == 'like' and not user_changed_utility:
             obj.set_like_or_dislike(is_like=True)
             obj.save()
+            track_action(request=request, action=ActionTrack.AttrAction.COMMENT_RATING_PLUS,
+                         page=ActionTrack.AttrPage.API_COMMENT_RATING,
+                         details=f'ip={get_client_ip(request)}, code_id={obj.pk}, u')
             response = JsonResponse({'status': 'Successful', 'state': 'like', 'rate': obj.rating})
             response.status_code = 200
             return response
-        elif action == 'dislike':
+        elif action == 'dislike' and not user_changed_utility:
             obj.set_like_or_dislike(is_dislike=True)
             obj.save()
+            track_action(request=request, action=ActionTrack.AttrAction.COMMENT_RATING_MINUS,
+                         page=ActionTrack.AttrPage.API_COMMENT_RATING,
+                         details=f'ip={get_client_ip(request)}, code_id={obj.pk}, u')
             response = JsonResponse({'status': 'Successful', 'state': 'dislike', 'rate': obj.rating})
+            response.status_code = 200
+            return response
+        elif action and user_changed_utility:
+            track_action(request=request, action_reason=ActionTrack.AttrReason.COMMENT_RATING_ALREADY_CHANGED,
+                         page=ActionTrack.AttrPage.API_COMMENT_RATING,
+                         action=ActionTrack.AttrAction.COMMENT_RATING_MINUS if action == 'dislike' else
+                         ActionTrack.AttrAction.COMMENT_RATING_PLUS)
+            logger.debug(f'Block get_comment_like_or_dislike_view_api Comment rating change unsuccessfully '
+                         f'user_changed_utility, post id={obj.pk}, from ip={get_client_ip(request)}')
+            response = JsonResponse({'rate': obj.rating, 'error': 'You already set like or dislike.'})
             response.status_code = 200
             return response
     except KeyError:
@@ -99,6 +194,5 @@ def get_comment_like_or_dislike_view_api(request):
         response = JsonResponse({'error': 'Unexpected error'})
         response.status_code = 400
     else:
-        response = JsonResponse({'status': 'Successful', 'state': 'You already set like or dislike'})
-        response.status_code = 200
-        return response
+        response = JsonResponse({'error': 'Unexpected action.'})
+        response.status_code = 400
