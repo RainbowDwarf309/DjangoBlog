449b73bf3d0e75649bbe5c9268580107ca4edbdd
rainbowdwarf <glubokov3099@gmail.com>
2023-04-20 17:57:26 +0700

    update views

--- blog/news/views.py
+++ blog/news/views.py
@@ -17,7 +17,8 @@ class HomeView(ListView):
     paginate_by = 4
 
     def get_queryset(self):
-        return Post.objects.filter(is_published=True).select_related('category', 'author').prefetch_related('tags')
+        return Post.objects.filter(is_published=True, status='APPROVED').select_related('category', 'author'). \
+            prefetch_related('tags')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -38,9 +39,6 @@ class SinglePostView(FormMixin, DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        self.object.views = F('views') + 1
-        self.object.save()
-        self.object.refresh_from_db()
         context['is_favorite'] = self.is_favorite()
         context['comments_count'] = Comment.objects.filter(post=Post.objects.select_related('author__userprofile').
                                                            get(slug=self.kwargs['slug'])).count()
