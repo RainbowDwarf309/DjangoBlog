0920f4ef1bd0b35fc42614856702bd6efad940ba
rainbowdwarf <glubokov3099@gmail.com>
2023-08-16 16:11:37 +0300

    update

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from dotenv import load_dotenv
 from celery.schedules import crontab
-from pathlib import Path
+
 
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
