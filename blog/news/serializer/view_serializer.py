59203c6e11b0dd6542af308a0acb7063a0e34770
Nikita <n.glubokov@eqwile.com>
2023-12-06 16:01:39 +0300

    add PostDetailViewSet

--- blog/news/serializer/view_serializer.py
+++ blog/news/serializer/view_serializer.py
@@ -1,15 +1,24 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .model_serializer import PostSerializer, CategorySerializer
 from news.models import Post, Category
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 
 
-class PostViewSet(ModelViewSet):
+class PostViewSet(ReadOnlyModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
 
 
 class CategoryViewSet(ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class PostDetailViewSet(APIView):
+
+    def get(self, slug):
+        post = Post.objects.get(slug=slug)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
