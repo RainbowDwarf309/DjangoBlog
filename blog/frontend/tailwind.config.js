87df9f60b4e899e2a80a82113bc4550acade21a7
Nikita <n.glubokov@eqwile.com>
2024-02-11 17:08:45 +0300

    add tailwind

---
+++ blog/frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      "green": "#c2f6ea"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
