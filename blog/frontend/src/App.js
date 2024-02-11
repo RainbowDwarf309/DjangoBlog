6e25607f32282effc5018c69c0e72869537c106c
Nikita <n.glubokov@eqwile.com>
2024-02-11 20:43:38 +0300

    update App.js

--- blog/frontend/src/App.js
+++ blog/frontend/src/App.js
@@ -1,27 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import Posts from "./components/Posts/Posts";
+import {Route, Routes} from 'react-router-dom'
+import {Navigation} from "./components/Navigation";
+import Posts from "./components/Posts";
+import Categories from "./components/Categories";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Posts/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navigation/>
+            <Routes>
+                <Route path="/" element={<Posts/>}/>
+                <Route path="/categories" element={<Categories/>}/>
+            </Routes>
+            {/*<Posts/>*/}
+        </div>
+    );
 }
 
 export default App;
