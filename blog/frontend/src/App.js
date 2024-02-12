e548f4343964f97b7be492a18c2f5de506a29417
Nikita <n.glubokov@eqwile.com>
2024-02-12 18:18:19 +0300

    update App

--- blog/frontend/src/App.js
+++ blog/frontend/src/App.js
@@ -3,6 +3,7 @@ import {Route, Routes} from 'react-router-dom'
 import {Navigation} from "./components/Navigation";
 import Posts from "./components/Posts";
 import Categories from "./components/Categories";
+import PostDetail from "./components/PostDeatil"
 
 
 function App() {
@@ -11,9 +12,9 @@ function App() {
             <Navigation/>
             <Routes>
                 <Route path="/" element={<Posts/>}/>
+                <Route path="/post/:slug" element={<PostDetail/>}/>
                 <Route path="/categories" element={<Categories/>}/>
             </Routes>
-            {/*<Posts/>*/}
         </div>
     );
 }
