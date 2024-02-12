0ed085ef4f3d4cc8650375cdb4f9f8d176fe2a6c
Nikita <n.glubokov@eqwile.com>
2024-02-12 22:08:27 +0300

    add CategoryDetail.jsx

---
+++ blog/frontend/src/components/CategoryDetail.jsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+import Posts from './Posts';
+
+import {HOST_URL} from '../constants';
+
+export function CategoryDetail() {
+    const {slug} = useParams();
+    const url = `${HOST_URL}/category_detail/${slug}/`;
+    const [posts, setPosts] = useState(null);
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(posts => setPosts(posts))
+            .catch(error => console.error('Ошибка при получении данных:', error));
+    }, []);
+
+    if (!posts) {
+        return <div>Загрузка...</div>;
+    }
+
+    return (
+        <div>
+            <Posts url={`/category_detail/${slug}/`}/>
+        </div>
+    )
+}
+
+export default CategoryDetail
