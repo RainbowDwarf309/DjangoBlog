6916289240d5efc04088c8636ef6565ba15a62e5
Nikita <n.glubokov@eqwile.com>
2024-02-13 18:07:14 +0300

    add drf authentication.py

---
+++ blog/news/authentication/authentication.py
@@ -0,0 +1,48 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.hashers import make_password
+from news.models import User
+
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if User.objects.filter(username=username).exists():
+            return Response({'message': 'User with this username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create(username=username, password=make_password(password))
+        user.save()
+
+        return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+
+
+class LoginViewDRF(APIView):
+    permission_classes = [AllowAny]
+
+    @staticmethod
+    def post(request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutViewDRF(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @staticmethod
+    def post(request):
+        logout(request)
+        return Response({'message': 'Logout successful'})
