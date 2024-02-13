9e9ffe498e5e28cb8f156960fb75daf92a0f8962
Nikita <n.glubokov@eqwile.com>
2024-02-13 18:09:27 +0300

    update model_serializer.py

--- blog/news/serializer/model_serializer.py
+++ blog/news/serializer/model_serializer.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
-from news.models import Post, Category, Tag, User
+from news.models import Post, Category, Tag, User, UserProfile
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser']
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserProfile
+        fields = ['user', 'avatar', 'bio', 'first_name', 'last_name', 'email', 'karma', 'monthly_karma']
 
 
 class CategorySerializer(serializers.ModelSerializer):
