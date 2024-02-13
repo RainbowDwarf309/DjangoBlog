371a5fa65b78b681a585951c3a3c394b9cba49af
Nikita <n.glubokov@eqwile.com>
2024-02-13 18:09:37 +0300

    update view_serializer.py

--- blog/news/serializer/view_serializer.py
+++ blog/news/serializer/view_serializer.py
@@ -2,8 +2,8 @@ from rest_framework.generics import RetrieveAPIView, ListAPIView
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .model_serializer import PostSerializer, CategorySerializer
-from news.models import Post, Category
+from .model_serializer import PostSerializer, CategorySerializer, UserSerializer, UserProfileSerializer
+from news.models import Post, Category, User, UserProfile
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 
 
@@ -29,3 +29,30 @@ class PostDetailViewSet(RetrieveAPIView):
     lookup_field = "slug"
     queryset = Post.objects.filter(is_published=True)
     serializer_class = PostSerializer
+
+
+class UserViewSet(ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
+
+
+class UserProfileViewSet(ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAdminUser]
+
+
+class UserProfileDetailViewSet(RetrieveAPIView):
+    lookup_field = "user"
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class TagDetailViewSet(ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        tag = self.kwargs['tag']
+        return Post.objects.filter(is_published=True, tags__slug=tag)
